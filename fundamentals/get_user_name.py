613e423c6098fa6880caed4ac2dad518b4439426
Dom Hallan <dom@polyglotdev.com>
2021-01-15 08:44:41 -0600

    :tada: add birth year question

--- fundamentals/get_user_name.py
+++ fundamentals/get_user_name.py
@@ -3,8 +3,10 @@
 
 def say_hello():
     get_name = input('What is your name?: ')
+    birth_year = input('What year were you born in?: ')
     length_of_name = len(get_name)
     print(f'Hello, {get_name}! Your name is {length_of_name} characters long.')
+    print(f'You are {2021 - int(birth_year)} years old')
 
 
 say_hello()
