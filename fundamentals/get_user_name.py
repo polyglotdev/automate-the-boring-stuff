73dc642a5f20f5a51b1d69c399a53fdd26649b3d
Dom Hallan <dom@polyglotdev.com>
2021-01-15 08:37:26 -0600

    :tada: add a few more expressions for len of name

--- fundamentals/get_user_name.py
+++ fundamentals/get_user_name.py
@@ -3,7 +3,8 @@
 
 def say_hello():
     get_name = input('What is your name?: ')
-    print(f'Hello, {get_name}!')
+    length_of_name = len(get_name)
+    print(f'Hello, {get_name}! Your name is {length_of_name} characters long.')
 
 
 say_hello()
