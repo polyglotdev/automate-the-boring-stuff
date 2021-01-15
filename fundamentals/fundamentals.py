6f3958d264bd6586a1a32fabe6039ee2676bfbc6
Dom Hallan <dom@polyglotdev.com>
2021-01-15 08:34:55 -0600

    :tada: complete section 1

--- fundamentals/fundamentals.py
+++ fundamentals/fundamentals.py
@@ -2,3 +2,14 @@
 print(2 + 2)
 print((5 * 2) + 1)
 print("mike" + " jones")
+
+# variables
+spam = 'eggs'
+eggs = 42
+bacon = 'bacon'
+
+print(spam)
+print(eggs)
+print(bacon)
+
+print(f'breakfast is {spam} and {bacon}')
---
+++ fundamentals/get_user_name.py
@@ -0,0 +1,9 @@
+# program says gets users' name and says hello
+
+
+def say_hello():
+    get_name = input('What is your name?: ')
+    print(f'Hello, {get_name}!')
+
+
+say_hello()
